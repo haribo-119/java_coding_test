d96e2f5bc47c5a1e380d13ea7d83e9fb875f17d1
haribo-119 <kangwyz1@gmail.com>
2024-06-16 16:59:31 +0900

    병합 정렬 코드 변경

--- src/MergeSort.java
+++ src/MergeSort.java
@@ -1,80 +1,65 @@
 public class MergeSort {
-    // 병합 정렬 함수
-    public static void mergeSort(int[] arr, int left, int right) {
-        if (left < right) {
-            // 중간 지점 계산
-            int mid = (left + right) / 2;
 
-            // 왼쪽 절반 정렬
-            mergeSort(arr, left, mid);
-            // 오른쪽 절반 정렬
-            mergeSort(arr, mid + 1, right);
-
-            // 두 절반을 병합
-            merge(arr, left, mid, right);
+    // 합병 정렬 메서드
+    public static void mergeSort(int[] array) {
+        if (array.length < 2) {
+            return;
         }
+        int mid = array.length / 2;
+        int[] left = new int[mid];
+        int[] right = new int[array.length - mid];
+
+        // 배열을 두 부분으로 분할
+        System.arraycopy(array, 0, left, 0, mid);
+        System.arraycopy(array, mid, right, 0, array.length - mid);
+
+        // 재귀적으로 정렬
+        mergeSort(left);
+        mergeSort(right);
+
+        // 두 부분을 병합
+        merge(array, left, right);
     }
 
-    // 병합 함수
-    public static void merge(int[] arr, int left, int mid, int right) {
-        // 두 부분 배열의 크기 계산
-        int n1 = mid - left + 1;
-        int n2 = right - mid;
+    // 병합 메서드
+    public static void merge(int[] array, int[] left, int[] right) {
+        int i = 0, j = 0, k = 0;
 
-        // 임시 배열 생성
-        int[] L = new int[n1];
-        int[] R = new int[n2];
-
-        // 데이터 복사
-        for (int i = 0; i < n1; ++i) {
-            L[i] = arr[left + i];
-        }
-        for (int j = 0; j < n2; ++j) {
-            R[j] = arr[mid + 1 + j];
-        }
-
-        // 임시 배열 병합
-        int i = 0, j = 0;
-        int k = left;
-        while (i < n1 && j < n2) {
-            if (L[i] <= R[j]) {
-                arr[k] = L[i];
-                i++;
+        // 두 부분 배열을 병합
+        while (i < left.length && j < right.length) {
+            if (left[i] <= right[j]) {
+                array[k++] = left[i++];
             } else {
-                arr[k] = R[j];
-                j++;
+                array[k++] = right[j++];
             }
-            k++;
         }
 
-        // 남은 요소들 복사
-        while (i < n1) {
-            arr[k] = L[i];
-            i++;
-            k++;
+        // 남은 원소 처리
+        while (i < left.length) {
+            array[k++] = left[i++];
         }
-
-        while (j < n2) {
-            arr[k] = R[j];
-            j++;
-            k++;
+        while (j < right.length) {
+            array[k++] = right[j++];
         }
     }
 
-    // 메인 함수
+    // 테스트 메서드
     public static void main(String[] args) {
-        int[] arr = {12, 11, 13, 5, 6, 7};
-        System.out.println("Unsorted array:");
-        for (int num : arr) {
-            System.out.print(num + " ");
+        int[] array = {12, 11, 13, 5, 6, 7};
+        System.out.println("주어진 배열");
+        printArray(array);
+
+        mergeSort(array);
+
+        System.out.println("정렬된 배열");
+        printArray(array);
+    }
+
+    // 배열 출력 메서드
+    public static void printArray(int[] array) {
+        for (int i : array) {
+            System.out.print(i + " ");
         }
         System.out.println();
-
-        mergeSort(arr, 0, arr.length - 1);
-
-        System.out.println("Sorted array:");
-        for (int num : arr) {
-            System.out.print(num + " ");
-        }
     }
 }
